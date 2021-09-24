01d547ea416d8846830572a3f66f5fab2a975187
NanyThery <nadinethery@gmail.com>
2021-09-24 21:24:59 +0200

    feat: testing deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import Head from "next/head"
 import Layout from "../components/Layout"
-import GlobalStyle from "../components/Theme/GlobalStyle"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <Head>
         <title>TeachT3ch</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
