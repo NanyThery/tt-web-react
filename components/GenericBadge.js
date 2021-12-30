0a4d72025009bad476e660d17daf7b21ed274a86
NanyThery <nadinethery@gmail.com>
2021-12-30 14:24:36 +0100

    add generic badge component

---
+++ components/GenericBadge.js
@@ -0,0 +1,39 @@
+import styled, { useTheme } from "styled-components";
+
+const Badge = styled.span`
+  font-size: 14px;
+  line-height: 24px;
+  color: ${(props) => props.variant};
+  border: 1px solid ${(props) => props.variant};
+  border-radius: 6px;
+  padding: 4px 6px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const GenericBadge = ({ variant, children }) => {
+  const theme = useTheme();
+
+  const getBadgeVariant = (variant) => {
+    switch (variant) {
+      case "purple":
+        return theme.colors.purple;
+      case "orange":
+        return theme.colors.orange;
+      case "yellow":
+        return theme.colors.yellow;
+      case "purpleDark":
+        return theme.colors.purpleDark;
+      case "purpleLight":
+        return theme.colors.purpleLight;
+      case "text80":
+        return theme.colors.text80;
+      case "text20":
+        return theme.colors.text20;
+      default:
+        return variant;
+    }
+  };
+
+  return <Badge variant={getBadgeVariant(variant)}>{children}</Badge>;
+};
