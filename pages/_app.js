9e949eecfe34a857ddfbc656292e82fa11a9d959
NanyThery <nadinethery@gmail.com>
2022-02-20 12:35:37 +0100

    add head with font import to document

--- pages/_app.js
+++ pages/_app.js
@@ -9,25 +9,12 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <title>TeachT3ch</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <GlobalStyle />
       <Theme>
-        <Head>
-          <title>TeachT3ch</title>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
         <Layout>
           <Component {...pageProps} isCourseOpen={isCourseOpen} />
         </Layout>
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -26,5 +26,28 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
