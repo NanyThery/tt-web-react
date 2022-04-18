b274fe250253807117d08aba2338ce9e2c89bcd5
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2022-04-18 20:49:28 +0000

    Change eventBar text

--- utils/eventBarInfo.js
+++ utils/eventBarInfo.js
@@ -2,8 +2,8 @@ export const eventBarInfo = {
   showUntilTimeStamp: 1652651999000,
   eventTitle: (
     <p>
-      ¡Atención! Próxima apertura de de inscripciones para la próxima edición
-      del 01/05/2022 al 15/05/2022
+      ¡Atención! Próxima apertura de inscripciones para la próxima edición del
+      01/05/2022 al 15/05/2022
     </p>
   ),
   eventLinkText: "Información del curso 👉",
