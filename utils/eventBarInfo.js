d9a9a427c8590a76a3038173c7bdb097bf1a69c1
Pablo Molina <contact@pablomolina.me>
2022-04-27 00:22:30 +0200

    update home banner

--- utils/eventBarInfo.js
+++ utils/eventBarInfo.js
@@ -3,7 +3,7 @@ export const eventBarInfo = {
   eventTitle: (
     <p>
       ¡Atención! Próxima apertura de inscripciones para la inminente edición del
-      01/05/2022 al 15/05/2022
+      02/05/2022 al 10/05/2022
     </p>
   ),
   eventLinkText: "Información del curso 👉",
