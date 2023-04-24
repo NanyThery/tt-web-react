b81b7607c114e1c7af2f3e8cae334c805672dbc9
NanyThery <nadinethery@gmail.com>
2023-04-24 18:44:07 +0200

    replace cards per same component as in the home for inscription

--- pages/curso/index.js
+++ pages/curso/index.js
@@ -3,8 +3,8 @@ import HeaderCurso from "../../components/Curso/HeaderCurso";
 import { cursoSection } from "../../utils/copies";
 import FAQSection from "../../components/FAQSection";
 import ContentCurso from "../../components/Curso/ContentCurso";
-import CourseCards from "../../components/Curso/CourseCards";
 import SectionSeparator from "../../components/SectionSeparator";
+import CourseSection from "../../components/Home/CourseSection";
 
 const Container = styled.div``;
 
@@ -16,7 +16,7 @@ export default function Curso({ isCourseOpen }) {
         description={cursoSection.header.description}
       ></HeaderCurso>
       <ContentCurso cursoInfo={cursoSection} isCourseOpen={isCourseOpen} />
-      <CourseCards isCourseOpen={isCourseOpen} />
+      <CourseSection isCourseOpen={isCourseOpen} />
       <SectionSeparator />
       <FAQSection />
       <SectionSeparator />
